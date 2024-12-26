f88c016ff8ed321fac41da430a997dd4b686c284
Fiona <shresthashuvanath@gmail.com>
2024-12-26 23:28:24 +0545

    Project files for v-1

---
+++ model/criminalmodel.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.criminals.model;
+
+/**
+ *
+ * @author user
+ */
+public class criminalmodel {
+    int Id;
+    String Name;
+    String Crime;
+    String Sentence;
+    public criminalmodel(int Id,String Name,String Crime,String Sentence){
+        this.Id = Id;
+        this.Name = Name;
+        this.Crime = Crime;
+        this.Sentence = Sentence;
+        
+    }
+public int getId(){
+    return Id;
+}
+public void setId(int Id) {
+    this.Id = Id;
+}
+
+public String getName(){
+    return Name;
+}
+public void setName(String Name){
+    this.Name = Name;
+}
+
+public String getCrime(){
+    return Crime;
+}
+public void setCrime(String Crime){
+    this.Crime = Crime;
+}
+
+public String getSentence(){
+    return Sentence;
+}
+public void setSentence(String Sentence){
+    this.Sentence = Name;
+}
+}
