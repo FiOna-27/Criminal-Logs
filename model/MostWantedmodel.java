8618e4ef2c6c9a56a325ed39537effa60f3bffd3
Fiona <shresthashuvanath@gmail.com>
2025-01-06 20:12:31 +0545

    new model for wanted table

---
+++ model/MostWantedmodel.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.criminals.model;
+
+/**
+ *
+ * @author user
+ */
+public class MostWantedmodel {
+    int Id;
+    String Name;
+    String Crime;
+    int Bounty;
+
+    public MostWantedmodel(int Id, String Name, String Crime, int Bounty) {
+        this.Id = Id;
+        this.Name = Name;
+        this.Crime = Crime;
+        this.Bounty = Bounty;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getCrime() {
+        return Crime;
+    }
+
+    public void setCrime(String Crime) {
+        this.Crime = Crime;
+    }
+
+    public int getBounty() {
+        return Bounty;
+    }
+
+    public void setBounty(int Bounty) {
+        this.Bounty = Bounty;
+    }
+    
+}
